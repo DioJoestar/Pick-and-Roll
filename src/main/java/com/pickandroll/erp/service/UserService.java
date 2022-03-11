efc636e31007f429397574677868d343b33b4537
Santiago Alves <alves.pizzorno.santiago.nahuel@alumnat.copernic.cat>
2022-03-11 09:36:50 +0100

    backend profile

--- src/main/java/com/pickandroll/erp/service/UserService.java
+++ src/main/java/com/pickandroll/erp/service/UserService.java
@@ -4,6 +4,7 @@ import com.pickandroll.erp.dao.UserDAO;
 import com.pickandroll.erp.model.Role;
 import com.pickandroll.erp.model.User;
 import java.util.ArrayList;
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
@@ -16,7 +17,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service("userDetailsService")
 @Slf4j
-public class UserService implements UserDetailsService {
+public class UserService implements UserDetailsService, userServiceInterface {
 
     @Autowired
     private UserDAO usuariDAO;
@@ -39,4 +40,32 @@ public class UserService implements UserDetailsService {
 
         return new org.springframework.security.core.userdetails.User(u.getName(), u.getPassword(), roles);
     }
+    
+    @Override
+    @Transactional
+    public void afegirUsuari(User user){
+        
+    }
+
+    @Override
+    @Transactional
+    public List<User> llistarUsuaris() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    @Transactional
+    public void eliminarUsuari(User user) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    @Transactional
+    public void editarUsuari(User user) {
+        
+        this.usuariDAO.save(user);
+        
+    }
+    
+    
 }
