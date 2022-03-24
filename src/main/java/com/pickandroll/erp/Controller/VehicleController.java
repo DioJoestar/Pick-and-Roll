18eca4ef4c3582e49bc3a9fd62811612a52aa769
Santiago Alves <alves.pizzorno.santiago.nahuel@alumnat.copernic.cat>
2022-03-24 12:38:55 +0100

    fixCart and fix modules

--- src/main/java/com/pickandroll/erp/Controller/VehicleController.java
+++ src/main/java/com/pickandroll/erp/Controller/VehicleController.java
@@ -1,9 +1,11 @@
 package com.pickandroll.erp.controller;
 
 import com.pickandroll.erp.dao.VehicleDAO;
+import com.pickandroll.erp.model.Cart;
 import com.pickandroll.erp.model.Vehicle;
 import com.pickandroll.erp.service.VehicleService;
 import com.pickandroll.erp.utils.Utils;
+import java.util.ArrayList;
 import java.util.List;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,10 +15,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class VehicleController {
+
+    public List<Vehicle> vehicles = new ArrayList<Vehicle>();
 
     @Autowired
     private VehicleDAO vehicleDao;
@@ -46,7 +51,7 @@ public class VehicleController {
     @PostMapping("/saveVehicle")
     public String saveData(@Valid Vehicle vehicle, Errors errors, Model model, RedirectAttributes msg) {
         Utils u = new Utils();
-        
+
         if (errors.hasErrors()) {
             List<Vehicle> vehicles = vehicleDao.findAll();
             model.addAttribute("vehicles", vehicles);
@@ -66,4 +71,48 @@ public class VehicleController {
         msg.addFlashAttribute("success", u.alert("profile.success"));
         return "redirect:/vehicles";
     }
+
+    @RequestMapping(value = "/addVehicle/{id}")
+    public String addVehicle(Vehicle v) {
+        v = vehicleService.findVehicle(v);
+        if(!vehicles.contains(v)){
+           vehicles.add(v);
+        }
+
+        return "redirect:/vehicles";
+    }
+
+    private Cart cart = new Cart();
+
+    @GetMapping("/cart")
+    public String cart(Model model) {
+
+        cart.setPriceU(vehicles);
+
+        cart.setSubPrice();
+        cart.setTotalPrice();
+
+        model.addAttribute("cart", cart);
+        model.addAttribute("vehicles", vehicles);
+        return "cart";
+    }
+
+    @RequestMapping(value = "/minus_day")
+    public String minusDay() {
+        cart.setDays(cart.getDays() - 1);
+        return "redirect:/cart";
+    }
+
+    @RequestMapping(value = "/plus_day")
+    public String plusDay() {
+        cart.setDays(cart.getDays() + 1);
+        return "redirect:/cart";
+    }
+
+    @RequestMapping(value = "/removeVehicle/{id}")
+    public String removeVehicle(Vehicle v) {
+        cart.removeVehicles(v, cart, vehicles);
+        return "redirect:/cart";
+    }
+
 }
