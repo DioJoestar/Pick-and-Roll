b3bc0bc66d25d5949d3f3a254b21e40a411331de
Xavi <xaviercarneplandolit@gmail.com>
2022-04-01 08:44:53 +0200

    OrderController and OrderDao fix

--- src/main/java/com/pickandroll/erp/controller/OrderController.java
+++ src/main/java/com/pickandroll/erp/controller/OrderController.java
@@ -1,7 +1,9 @@
 package com.pickandroll.erp.controller;
 
 import com.pickandroll.erp.model.Order;
+import com.pickandroll.erp.model.Vehicle;
 import com.pickandroll.erp.service.OrderService;
+import com.pickandroll.erp.service.VehicleService;
 import com.pickandroll.erp.utils.Utils;
 import java.util.List;
 import javax.validation.Valid;
@@ -16,35 +18,41 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class OrderController {
-
+    
     @Autowired
     private OrderService orderService;
+    
+    @Autowired
+    private VehicleService vehicleService;
 
     @GetMapping("/orders")
-    public String users(Order order, Model model) {
+    public String orders(Order order, Model model) {
 
         List<Order> orders = orderService.listOrders();
         model.addAttribute("orders", orders);
+        
+        List<Vehicle> vehicles = vehicleService.listVehicles();
+        model.addAttribute("vehicles", vehicles);
 
         return "orders";
     }
 
     @PostMapping("/editOrder")
     public String editOrder(@ModelAttribute Order order, Model model) {
-        
         List<Order> orders = orderService.listOrders();
         model.addAttribute("orders", orders);
 
         order = orderService.findById(order.getId());
+
+        //List<Vehicle> listVehicles = vehicleService.listVehicles();
+
         model.addAttribute("order", order);
+        //model.addAttribute("listVehicles", listVehicles);
         return "orders";
     }
 
     @PostMapping("/saveOrder")
-    public String saveData(@Valid Order order, Errors errors, Model model, RedirectAttributes msg) {
-        
-        Utils u = new Utils();
-        
+    public String saveOrder(@Valid Order order, Errors errors, Model model, RedirectAttributes msg) {
         if (errors.hasErrors()) {
             List<Order> orders = orderService.listOrders();
             model.addAttribute("orders", orders);
@@ -52,6 +60,14 @@ public class OrderController {
         }
 
         orderService.addOrder(order);
+        return "redirect:/orders";
+    }
+
+    @PostMapping("/deleteOrder")
+    public String deleteOrder(@Valid Order order, Errors errors, RedirectAttributes msg) {
+        Utils u = new Utils();
+
+        orderService.deleteOrder(order);
         msg.addFlashAttribute("success", u.alert("profile.success"));
         return "redirect:/orders";
     }
