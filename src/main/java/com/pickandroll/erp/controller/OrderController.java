ff701fb54bc231c250ae5c718ffea5f08538449c
Santiago Alves <alves.pizzorno.santiago.nahuel@alumnat.copernic.cat>
2022-04-06 11:46:12 +0200

    enable vehicle order

--- src/main/java/com/pickandroll/erp/controller/OrderController.java
+++ src/main/java/com/pickandroll/erp/controller/OrderController.java
@@ -5,6 +5,7 @@ import com.pickandroll.erp.model.User;
 import com.pickandroll.erp.model.Vehicle;
 import com.pickandroll.erp.service.OrderService;
 import com.pickandroll.erp.service.UserService;
+import com.pickandroll.erp.service.VehicleService;
 import java.util.ArrayList;
 import java.util.List;
 import javax.validation.Valid;
@@ -26,6 +27,9 @@ public class OrderController {
     private OrderService orderService;
 
     @Autowired
+    private VehicleService vehicleService;
+
+    @Autowired
     private UserService userService;
 
     @GetMapping("/orders")
@@ -33,13 +37,13 @@ public class OrderController {
 
         // Coger los datos de la sessión
         UserDetails userDetails = (UserDetails) auth.getPrincipal();
-        
+
         // Crear l'objecte User a partir del email (username) de la sessió actual
         User currUser = userService.findByEmail(userDetails.getUsername());
         List<Order> userOrders = new ArrayList<>();
-        
+
         // Assignar una llista de comandes diferentes depenent de l'usuari
-        if("admin".equals(currUser.getRoles().get(0).getName())) {
+        if ("admin".equals(currUser.getRoles().get(0).getName())) {
             userOrders = orderService.listOrders();
         } else {
             userOrders = currUser.getOrders();
@@ -54,7 +58,7 @@ public class OrderController {
 
         List<Order> orders = orderService.listOrders();
         model.addAttribute("orders", orders);
-        
+
         // Obté l'ordre desitjada a partir de la id d'aquesta
         order = orderService.findById(order.getId());
         model.addAttribute("order", order);
@@ -63,16 +67,18 @@ public class OrderController {
     }
 
     @PostMapping("/saveOrder")
-    public String saveOrder(@Valid Order order, Errors errors, Model model, 
+    public String saveOrder(@Valid Order order, Errors errors, Model model,
             RedirectAttributes msg) {
-        
+
+        for (int i = 0; i < order.getVehicles().size(); i++) {
+            order.getVehicles().get(i).setEnabled(order.isReturned());
+            Vehicle v = vehicleService.findById(order.getVehicles().get(i).getId());
+            vehicleService.addVehicle(v);
+        }
+
         // Guarda l'ordre editada amb les dades introduides
         orderService.addOrder(order);
-        
-        for(Vehicle v : order.getVehicles()){
-            v.setEnabled(order.isReturned());
-        }
-        
+
         return "redirect:/orders";
     }
 }
