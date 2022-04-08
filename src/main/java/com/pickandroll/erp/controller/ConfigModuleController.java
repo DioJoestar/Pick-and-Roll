35dcb7c0d398779cbc4f3a3c89c11503f66e9214
Xavi <xaviercarneplandolit@gmail.com>
2022-04-08 09:13:05 +0200

    code commented and cleaned a little bit

--- src/main/java/com/pickandroll/erp/controller/ConfigModuleController.java
+++ src/main/java/com/pickandroll/erp/controller/ConfigModuleController.java
@@ -1,6 +1,5 @@
 package com.pickandroll.erp.controller;
 
-import com.pickandroll.erp.dao.ModuleDAO;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
